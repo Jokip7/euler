7d48038a33e35bf7fd391d8eb56e5a5d22d2e211
Jokip7 <jokip7@gmail.com>
2022-09-26 13:08:17 +0200

    Fixed mistake in is_prime function

--- euler3.py
+++ euler3.py
@@ -9,7 +9,7 @@ TARGET = 600851475143
 
 
 def is_prime(num):
-    for i in range(2, int(math.sqrt(num))):
+    for i in range(2, int(math.sqrt(num) + 1)):
         if num % i == 0:
             return False
     return True
